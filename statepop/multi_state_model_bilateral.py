49bee5ed008cf96e0540b0582f9a37387ca3ee2b
cdeciampa <corrine.deciampa@pnnl.gov>
2020-08-03 13:28:10 -0700

    minor changes

--- statepop/multi_state_model_bilateral.py
+++ statepop/multi_state_model_bilateral.py
@@ -1,5 +1,6 @@
 import pkg_resources
 import os
+import numpy as np
 import pandas as pd
 import matplotlib
 import statepop.multistate as multi
@@ -43,8 +44,8 @@ useBrassf <- T # TRUE (use Brass); FALSE (use scaling)
 
 """""""""""""""""""""""""""""""""
 #* Generate Directories
-for results in results_path:
-    os.makedirs(results, exist_ok=True)
+#for results in results_path:
+#    os.makedirs(results, exist_ok=True)
 
 mort_30 = pd.read_csv(mortality_30)
 mort_100 = pd.read_csv(mortality_100)
@@ -77,8 +78,67 @@ total_international_migration = pd.DataFrame(index = 4*num_ages*(steps+1), colum
 #############################################
 #*** Generate baseline population matrix ***#
 #############################################
+def get_state_dirs(root_dir, delimeter='-', target_index=0):
+    state_dirs = []
+    for i in os.listdir(root_dir):
+        split_i = i.split("-")[0]
 
-# Loop over regions to update their base year population with international and state-level migrations
-for states in regions:
-    # * Generate paths
-    path_in = state_inputs[states]
+        try:
+            x = int(split_i)
+            state_dirs.append(os.path.join(root_dir, i))
+        except ValueError:
+            pass
+    return state_dirs
+
+def get_contents_file_list(state_dirs, target_file):
+
+    constant_rate_files = []
+    for i in state_dirs:
+
+        contents = os.listdir(i)
+        if target_file in contents:
+            constant_rate_files.append(os.path.join(i, target_file))
+
+        else:
+            print(f"No contents file in directory {i}")
+
+    return constant_rate_files
+
+def make_dataframes(file_list):
+    dfs = [pd.read_csv(files) for files in file_list]
+    return dfs
+
+#* Generate paths
+state_dir_list = get_state_dirs(state_inputs) # Input data directory
+
+#* Scenario data (The Constant_rate scenario.)
+constant_rates = get_contents_file_list(state_dir_list, target_file='Constant_rate.csv')
+constant_rate_dfs = make_dataframes(constant_rates)
+
+#* Base Population data
+base_population = get_contents_file_list(state_dir_list, target_file='basePop.csv')
+base_population_dfs = make_dataframes(base_population)
+
+# Variables
+base_population_age = "age"                                 # Age categories (numeric, starting with 0)
+base_population_rural_female = "rural_female"               # Rural female population
+base_population_urban_female = "urban_female"               # Urban female population
+base_population_rural_male = "rural_male"                   # Rural male population
+base_population_urban_male = "urban_male"                   # Urban male population
+
+#* International migration data
+# Note: The international migration rates input data needs to be in relative rates (summing up to 1)
+international_migration_rates = get_contents_file_list(state_dir_list, target_file='intMig.csv')
+internation_migration_rate_dfs = make_dataframes(international_migration_rates)
+
+# Variables
+net_migration_age = "age"                       # Age variable
+net_migration_female = "net_female"             # Proportion of net migration for each age group for females
+net_migration_male = "net_male"                 # Proportion of net migration for each age group for males
+
+# Annual total net international migration counts for 5-year periods
+female_net_internation_migration = constant_rate_dfs[0].nim_F.values() if #in range of years specified?
+male_net_international_migration =
+
+nMigFt < - as.numeric(scenario[scenario$year % in % c(yearStart: yearEnd), "nim_F"])  # female net international migrants
+nMigMt < - as.numeric(scenario[scenario$year % in % c(yearStart: yearEnd), "nim_M"])  # male net international migrants
