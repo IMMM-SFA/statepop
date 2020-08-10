6aceb400d929f87f641350e81272a5f3bf7a4424
cdeciampa <corrine.deciampa@pnnl.gov>
2020-08-10 12:52:12 -0700

    made changes to debugging.ipynb

--- statepop/multi_state_model_bilateral.py
+++ statepop/multi_state_model_bilateral.py
@@ -26,6 +26,52 @@ regions = os.listdir(state_inputs)
 # Specify scenario
 scenarios = ("Constant_rate", "SSP2", "SSP3", "SSP5")
 
+
+def get_state_dirs(root_dir, delimeter='-', target_index=0):
+    """Get a list of the full file path to the state directories
+
+    :param root_dir                 A full path with file name and extension to the directory containing the state level data
+
+    :type root_dir:                 str
+
+    :param ..:
+
+    :return:                        A list of full path...
+
+    """
+
+    state_dirs = []
+    for i in os.listdir(root_dir):
+        split_i = i.split("-")[0]
+
+        try:
+            x = int(split_i)
+            state_dirs.append(os.path.join(root_dir, i))
+        except ValueError:
+            pass
+
+    return state_dirs
+
+
+def get_contents_file_list(state_dirs, target_file):
+    constant_rate_files = []
+    for i in state_dirs:
+
+        contents = os.listdir(i)
+        if target_file in contents:
+            constant_rate_files.append(os.path.join(i, target_file))
+
+        else:
+            print(f"No contents file in directory {i}")
+
+    return constant_rate_files
+
+
+def make_dataframes(file_list):
+    dfs = [pd.read_csv(files, skiprows=3) for files in file_list]
+
+    return dfs
+
 """"""""""""""""""""""""""""""""""
 # Not sure how to directly incorporate this, come back to it later
 
@@ -51,7 +97,7 @@ mort_30 = pd.read_csv(mortality_30)
 mort_100 = pd.read_csv(mortality_100)
 
 #From 0 to 100
-num_ages = [i for i in range(101)]
+num_ages = list(range(101))
 
 initial_all_base_population = pd.DataFrame(index = 4*num_ages, columns=regions)
 updated_all_base_population = pd.DataFrame(index = 4*num_ages, columns=regions)
@@ -59,6 +105,7 @@ updated_all_base_population = pd.DataFrame(index = 4*num_ages, columns=regions)
 #* Define starting year and end year
 year_start = 2010
 year_through = 2100
+timestep = 5
 steps = year_through - year_start
 
 # These dataframes will hold population projections and net/in/out state-level migrations for all states and years
@@ -78,58 +125,30 @@ total_international_migration = pd.DataFrame(index = 4*num_ages*(steps+1), colum
 #############################################
 #*** Generate baseline population matrix ***#
 #############################################
-def get_state_dirs(root_dir, delimeter='-', target_index=0):
-    state_dirs = []
-    for i in os.listdir(root_dir):
-        split_i = i.split("-")[0]
 
-        try:
-            x = int(split_i)
-            state_dirs.append(os.path.join(root_dir, i))
-        except ValueError:
-            pass
-    return state_dirs
-
-def get_contents_file_list(state_dirs, target_file):
-
-    constant_rate_files = []
-    for i in state_dirs:
-
-        contents = os.listdir(i)
-        if target_file in contents:
-            constant_rate_files.append(os.path.join(i, target_file))
-
-        else:
-            print(f"No contents file in directory {i}")
-
-    return constant_rate_files
-
-def make_dataframes(file_list):
-    dfs = [pd.read_csv(files) for files in file_list]
-    return dfs
 
 #* Generate paths
 state_dir_list = get_state_dirs(state_inputs) # Input data directory
 
 #* Scenario data (The Constant_rate scenario.)
 constant_rates = get_contents_file_list(state_dir_list, target_file='Constant_rate.csv')
-constant_rate_dfs = make_dataframes(constant_rates)
+#constant_rate_dfs = make_dataframes(constant_rates)
 
 #* Base Population data
 base_population = get_contents_file_list(state_dir_list, target_file='basePop.csv')
-base_population_dfs = make_dataframes(base_population)
+#base_population_dfs = make_dataframes(base_population)
 
-# Variables
-base_population_age = "age"                                 # Age categories (numeric, starting with 0)
-base_population_rural_female = "rural_female"               # Rural female population
-base_population_urban_female = "urban_female"               # Urban female population
-base_population_rural_male = "rural_male"                   # Rural male population
-base_population_urban_male = "urban_male"                   # Urban male population
+# Variables (already in dataframe as columns, come back to fix later)
+#base_population_age = "age"                                 # Age categories (numeric, starting with 0)
+#base_population_rural_female = "rural_female"               # Rural female population
+#base_population_urban_female = "urban_female"               # Urban female population
+#base_population_rural_male = "rural_male"                   # Rural male population
+#base_population_urban_male = "urban_male"                   # Urban male population
 
 #* International migration data
 # Note: The international migration rates input data needs to be in relative rates (summing up to 1)
 international_migration_rates = get_contents_file_list(state_dir_list, target_file='intMig.csv')
-internation_migration_rate_dfs = make_dataframes(international_migration_rates)
+#internation_migration_rate_dfs = make_dataframes(international_migration_rates)
 
 # Variables
 net_migration_age = "age"                       # Age variable
@@ -137,8 +156,6 @@ net_migration_female = "net_female"             # Proportion of net migration fo
 net_migration_male = "net_male"                 # Proportion of net migration for each age group for males
 
 # Annual total net international migration counts for 5-year periods
-female_net_internation_migration = constant_rate_dfs[0].nim_F.values() if #in range of years specified?
-male_net_international_migration =
+scenario_df = scenario_df.loc[scenario_df["year"].isin(list(range((year_start, year_through + timestep, timestep))]
 
-nMigFt < - as.numeric(scenario[scenario$year % in % c(yearStart: yearEnd), "nim_F"])  # female net international migrants
-nMigMt < - as.numeric(scenario[scenario$year % in % c(yearStart: yearEnd), "nim_M"])  # male net international migrants
+### Continued on debugging.ipynb for now ###
