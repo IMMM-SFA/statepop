01f96f9eedb71e1d7e43692b9c4b46bf44dfb79d
cdeciampa <corrine.deciampa@pnnl.gov>
2020-08-20 18:06:32 -0700

    moved No_Mig files and made progress

--- statepop/multi_state_model_bilateral.py
+++ statepop/multi_state_model_bilateral.py
@@ -5,23 +5,15 @@ import pandas as pd
 import matplotlib
 import statepop.multistate as multi
 
-# Path to results folder (ask Chris how to handle)
+#Path to results folder
 results_path = pkg_resources.resource_filename('statepop', 'data/inputs/No_Mig')
 
 # Path to state-level inputs folders
 state_inputs = pkg_resources.resource_filename('statepop', 'data/State_Inputs')
 
-# Path to the package
-# potentially have an absolute import statement above ? Need to recreate package.
-
-# UN standard life table e0=30; used for linear interpolation of lx values
-mortality_30 = pkg_resources.resource_filename('statepop', 'data/Inputs/AllRegions_mortality_UNe030.csv')
-
-# UN standard life table e0=100; used for linear interpolation of lx values
-mortality_100 = pkg_resources.resource_filename('statepop', 'data/Inputs/AllRegions_mortality_UNe0100.csv')
-
-# Specify regions
-regions = os.listdir(state_inputs)
+# List of states, alphebetized
+states_list = os.listdir(state_inputs)
+states_list = sorted(states_list, key = lambda x: int(x.split('-')[0]))
 
 # Specify scenario
 scenarios = ("Constant_rate", "SSP2", "SSP3", "SSP5")
@@ -67,95 +59,143 @@ def get_contents_file_list(state_dirs, target_file):
     return constant_rate_files
 
 
-def make_dataframes(file_list):
-    dfs = [pd.read_csv(files, skiprows=3) for files in file_list]
+def retrieve_csv(state_abbr, target_file):
+    state_dir_list = get_state_dirs(state_inputs)  # Input data directory
+    target_state = [s for s in states_list if state_abbr in s]
 
-    return dfs
+    if target_file == 'Constant_rate.csv':
+        file_list = get_contents_file_list(state_dir_list, target_file=target_file)
+        file = [i for i in file_list if target_state[0] in i][0]
 
-""""""""""""""""""""""""""""""""""
-# Not sure how to directly incorporate this, come back to it later
+        df = pd.read_csv(file, skiprows=[1, 2, 3])
 
-# Specify the domestic migration factor
-# If scenario is not "Constant_rate" (for fertility, mortality and international migration), this factor will become dynamic later
-scen.factor <- 0 # 1 for regular, 0 for no domestic migration, 0.5 for half scenario and 2 for double scenario
+    elif target_file == 'mortality.csv':
+        file_list = get_contents_file_list(state_dir_list, target_file=target_file)
+        file = [i for i in file_list if target_state[0] in i][0]
 
-# Sepecify if international migration is applied
-int.mig <- 0 # 1 applied 0 not applied
+        df = pd.read_csv(file, usecols=['region', 'age', 'lx_rural_female',
+                                        'lx_rural_male', 'lx_urban_female', 'lx_urban_male'])
 
-#* Should details for projection model adjustment be printed?
-vis <- F # TRUE (print details); FALSE (don't print details)
+    else:
+        file_list = get_contents_file_list(state_dir_list, target_file=target_file)
+        file = [i for i in file_list if target_state[0] in i][0]
 
-#* Should the Brass Relational Model be used or a simple scaling approach to compute future fertility schedules
-useBrassf <- T # TRUE (use Brass); FALSE (use scaling)
+        df = pd.read_csv(file)
 
-"""""""""""""""""""""""""""""""""
-#* Generate Directories
-#for results in results_path:
-#    os.makedirs(results, exist_ok=True)
-
-mort_30 = pd.read_csv(mortality_30)
-mort_100 = pd.read_csv(mortality_100)
-
-#From 0 to 100
-num_ages = list(range(101))
-
-initial_all_base_population = pd.DataFrame(index = 4*num_ages, columns=regions)
-updated_all_base_population = pd.DataFrame(index = 4*num_ages, columns=regions)
-
-#* Define starting year and end year
-year_start = 2010
-year_through = 2100
-timestep = 5
-steps = year_through - year_start
-
-# These dataframes will hold population projections and net/in/out state-level migrations for all states and years
-total_population_projection = pd.DataFrame()
-total_state_net_migration = pd.DataFrame()
-total_state_in_migration = pd.DataFrame()
-total_state_out_migration = pd.DataFrame()
-
-# This dataframe holds population values before applying domestic migration. It is necessary to disaggregate
-# domestic migration to/from each state across all other states
-total_population_no_domestic_migration = pd.DataFrame()
-
-# Initialize a dataframe for holding international migration for all years and states
-total_international_migration = pd.DataFrame(index = 4*num_ages*(steps+1), columns=regions)
+    return df
 
 
-#############################################
-#*** Generate baseline population matrix ***#
-#############################################
+def single_state_files(state_abbr, target_file, year_start=2010, year_through=2100, timestep=5):
+    """Retrieve indicated state's constant rate population scenario file and assign to dataframe.
+
+    :param state_abbr:              Two-character state abbreviations (ex: AL, AK, AZ, etc.)
+
+    :type state_abbr:               str
+
+    :param target_file:             CSV file being retrieved for specified state
+
+    :type state_abbr:               str
+
+    :param year_start:              (Optional)
+
+    :param year_through:            (Optional)
+
+    :param timestep:                (Optional) Years are by 5
 
 
-#* Generate paths
-state_dir_list = get_state_dirs(state_inputs) # Input data directory
 
-#* Scenario data (The Constant_rate scenario.)
-constant_rates = get_contents_file_list(state_dir_list, target_file='Constant_rate.csv')
-#constant_rate_dfs = make_dataframes(constant_rates)
+    :return:                        Targeted CSV as a pandas DataFrame
 
-#* Base Population data
-base_population = get_contents_file_list(state_dir_list, target_file='basePop.csv')
-#base_population_dfs = make_dataframes(base_population)
+    """
 
-# Variables (already in dataframe as columns, come back to fix later)
-#base_population_age = "age"                                 # Age categories (numeric, starting with 0)
-#base_population_rural_female = "rural_female"               # Rural female population
-#base_population_urban_female = "urban_female"               # Urban female population
-#base_population_rural_male = "rural_male"                   # Rural male population
-#base_population_urban_male = "urban_male"                   # Urban male population
+    if target_file == 'Constant_rate.csv':
 
-#* International migration data
-# Note: The international migration rates input data needs to be in relative rates (summing up to 1)
-international_migration_rates = get_contents_file_list(state_dir_list, target_file='intMig.csv')
-#internation_migration_rate_dfs = make_dataframes(international_migration_rates)
+        target_df = retrieve_csv(state_abbr, target_file)
 
-# Variables
-net_migration_age = "age"                       # Age variable
-net_migration_female = "net_female"             # Proportion of net migration for each age group for females
-net_migration_male = "net_male"                 # Proportion of net migration for each age group for males
+        target_df = target_df.loc[target_df["year"].isin(
+            list(range(year_start, year_through + timestep, timestep)))]
 
-# Annual total net international migration counts for 5-year periods
-scenario_df = scenario_df.loc[scenario_df["year"].isin(list(range((year_start, year_through + timestep, timestep))]
+    elif target_file == 'basePop.csv':
+        target_df = retrieve_csv(state_abbr, target_file)
 
-### Continued on debugging.ipynb for now ###
+    elif target_file == 'intMig.csv':
+        target_df = retrieve_csv(state_abbr, target_file)
+
+    elif target_file == 'mortality.csv':
+        target_df = retrieve_csv(state_abbr, target_file)
+
+    elif target_file == 'fertility.csv':
+        target_df = retrieve_csv(state_abbr, target_file)
+
+    elif target_file == 'domMig.csv':
+        target_df = retrieve_csv(state_abbr, target_file)
+
+    return target_df
+
+
+def single_state_base_pop_df(state_abbr, int_mig_rates=0):
+    # * Scenario data (The Constant_rate scenario. )
+    cr_df = single_state_files(state_abbr, 'Constant_rate.csv')
+
+    # * Base Population data
+    bp_df = single_state_files(state_abbr, 'basePop.csv')
+
+    # * International migration data
+    # Note: The international migration rates input data needs to be in relative rates (summing up to 1)
+    im_df = single_state_files(state_abbr, 'intMig.csv')
+
+    # Annual total net international migration counts for 5-year periods
+    female_net_international_migration = cr_df["nim_F"].round(0).astype(np.int).values
+    male_net_international_migration = cr_df["nim_M"].round(0).astype(np.int).values
+
+    # Update the base year population with the international migration
+    # females -> urban0, rural0, urban1, rural1,...
+    females_base_pop = pd.concat([bp_df['urban_female'], bp_df['rural_female']]).sort_index(kind='mergesort')
+    females_base_pop = females_base_pop.reset_index(drop=True)
+
+    # males
+    males_base_pop = pd.concat([bp_df['urban_male'], bp_df['rural_male']]).sort_index(kind='mergesort')
+    males_base_pop = males_base_pop.reset_index(drop=True)
+
+    # combines female and male pieces
+    base_pop_df = pd.DataFrame(pd.concat([females_base_pop,
+                                          males_base_pop]).reset_index(drop=True))
+    base_pop_df = base_pop_df.fillna(0)
+    base_pop_df = base_pop_df.round(0).astype(np.int)
+    base_pop_df.columns = [s for s in states_list if state_abbr in s]
+
+    # Spread migrant numbers according to profile
+    if int_mig_rates == 1:
+        im_df["nmUF"] = im_df['net_female'] * female_net_international_migration[0]
+        im_df["nmRF"] = im_df['nmUF']
+        im_df["nmUM"] = im_df['net_male'] * male_net_international_migration[0]
+        im_df['nmRM'] = im_df['nmUM']
+    else:
+        im_df["nmUF"] = im_df['net_female'] * 0
+        im_df["nmRF"] = im_df['nmUF']
+        im_df["nmUM"] = im_df['net_male'] * 0
+        im_df['nmRM'] = im_df['nmUM']
+
+    # Update the base year population with the international migration rates
+    urban_rural_females = pd.concat([im_df['nmUF'], im_df['nmRF']]).sort_index(kind='mergesort')
+    urban_rural_females = urban_rural_females.reset_index(drop=True)
+
+    urban_rural_males = pd.concat([im_df['nmUM'], im_df['nmRM']]).sort_index(kind='mergesort')
+    urban_rural_males = urban_rural_males.reset_index(drop=True)
+
+    urban_rural_all = pd.DataFrame(pd.concat([urban_rural_females,
+                                              urban_rural_males]).reset_index(drop=True))
+    urban_rural_all.columns = [s for s in states_list if state_abbr in s]
+
+    # Populate the data frames that store base-year population and its updated population by international migration
+    initial_all_base_population = pd.DataFrame(data=base_pop_df, index=list(range(404)),
+                                               columns=states_list).fillna(0)
+    update_all_base_pop = base_pop_df.add(urban_rural_all, fill_value=0)
+
+    # Keep the international migration for the current state
+    current_int_migration = update_all_base_pop.subtract(base_pop_df, fill_value=0)
+    total_int_migration = pd.DataFrame(data=current_int_migration, index=list(range(404)), columns=states_list)
+
+    return total_int_migration
+
+
